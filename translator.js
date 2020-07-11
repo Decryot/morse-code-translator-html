69a2021cf6a9cff67de7d5edd4ebbbe740db46a8
Dmitri <56200546+Decryot@users.noreply.github.com>
2020-07-11 17:18:10 -0400

    added source

---
+++ translator.js
@@ -0,0 +1,54 @@
+let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+  
+let morseCode = [
+".- ",
+"-... ",
+"-.-. ",
+"-.. ",
+". ",
+"..-. ",
+"--. ",
+".... ",
+".. ",
+".--- ",
+"-.- ",
+".-.. ",
+"-- ",
+"-. ",
+"--- ",
+".--. ",
+"--.- ",
+".-. ",
+"... ",
+"- ",
+"..- ",
+"...- ",
+".-- ",
+"-..- ",
+"-.-- ",
+"--.. ",
+"/ "]
+
+function translateIntoMorseCode(inputText) {
+    var chars = [inputText.length]
+    var morseCodeText = "";
+    chars = Array.from(inputText)
+
+    for (var msgChars = 0; msgChars < chars.length; msgChars++) {
+        for (var letterId = 0; letterId < letters.length; letterId++) {
+            if (chars[msgChars].toLowerCase() == letters[letterId]) {
+                morseCodeText += morseCode[letterId];
+            }
+        }
+    }
+    return morseCodeText;
+}
+
+function onInputChange() {
+    console.log("a")
+    var textInput = document.getElementById("input")
+    var morseCodeOutput = document.getElementById("morse-code-output")
+    var morseCodeText = translateIntoMorseCode(textInput.value);
+
+    morseCodeOutput.textContent = morseCodeText;
+}
